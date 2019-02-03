851ce186c62a71b3ea8107e25f037fb9a00155de
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-02-02 18:54:31 -0600

    Data rendering to the DOM

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+            let temperatureDescription = document.querySelector('.temperature-description');
+            let temperatureDegree = document.querySelector('.temperature-degree');
+            let locationTimezone = document.querySelector('.location-timezone');
 
             const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}You api key here${lat},${long}`
+            const api = `${proxy}Your api here${lat},${long}`
     
             fetch(api)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
-                    console.log(data);
+                    const {temperature, summary } = data.currently;
+                    //set the dom elements from the api
+                    temperatureDegree.textContent = temperature
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
                 })
         })
     }
